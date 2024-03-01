d33fd0a48a4739487f38c1bb697b8c70516108d0
Tom Miller <tommmmm@protonmail.com>
2024-03-01 00:31:44 +0000

    Add pinning

--- src/main.zig
+++ src/main.zig
@@ -2,39 +2,30 @@ const std = @import("std");
 const vec = @import("vec.zig");
 const GFX = @import("gfx.zig").GFX;
 
-pub const Particle = struct {
-    pos: vec.Vec2,
-    vel: vec.Vec2,
-
-    pub fn init(pos: vec.Vec2, vel: vec.Vec2) Link {
-        return Link{
-            .pos = pos,
-            .vel = vel,
-        };
-    }
-
-    pub fn accTowardsRadius(self: Particle, other: Particle, other_radius: f64, proportion: f64) void {
-        _ = self;
-        _ = other;
-        _ = other_radius;
-        _ = proportion;
-        // dif = other.pos - self.pos
-        // self.vel += dif.normalise() * (math.sqrt(dif.x ** 2 + dif.y ** 2) - other_radius) * proportion
-    }
-};
-
 pub const Link = struct {
     length: f32,
 
     pub fn init(length: ?f32) Link {
-        return Link{
-            .length = length orelse 1,
+        return .{
+            .length = length orelse 0.1,
+        };
+    }
+};
+
+pub const Point = struct {
+    vel: vec.Vec2,
+    pin: bool,
+
+    pub fn init(vel: ?vec.Vec2, pin: ?bool) Point {
+        return .{
+            .vel = vel orelse .{ 0, 0 },
+            .pin = pin orelse false,
         };
     }
 };
 
 pub const Net = struct {
-    // the net contains particles and links between them
+    // the net contains points and links between them
     // in this example they are arranged in a grid
     width: u16 = 20,
     height: u16 = 20,
@@ -44,25 +35,25 @@ pub const Net = struct {
 
     links: std.ArrayList(Link),
     link_indices: std.ArrayList(c_uint),
-    positions: std.ArrayList(vec.Vec2),
-    velocities: std.ArrayList(vec.Vec2),
+    points: std.ArrayList(Point),
+    point_positions: std.ArrayList(vec.Vec2),
 
     pub fn init(alloc: std.mem.Allocator) !Net {
         var net = Net{
             .links = undefined,
             .link_indices = undefined,
-            .positions = undefined,
-            .velocities = undefined,
+            .points = undefined,
+            .point_positions = undefined,
         };
 
         net.links = @TypeOf(net.links).init(alloc);
         errdefer net.links.deinit();
         net.link_indices = @TypeOf(net.link_indices).init(alloc);
         errdefer net.link_indices.deinit();
-        net.positions = @TypeOf(net.positions).init(alloc);
-        errdefer net.positions.deinit();
-        net.velocities = @TypeOf(net.velocities).init(alloc);
-        errdefer net.velocities.deinit();
+        net.points = @TypeOf(net.points).init(alloc);
+        errdefer net.points.deinit();
+        net.point_positions = @TypeOf(net.point_positions).init(alloc);
+        errdefer net.point_positions.deinit();
 
         try net.link_indices.appendSlice(&[_]c_uint{
             1, 2,
@@ -71,7 +62,7 @@ pub const Net = struct {
             4, 5,
         });
 
-        try net.positions.appendSlice(&[_]vec.Vec2{
+        try net.point_positions.appendSlice(&[_]vec.Vec2{
             .{ -0.5, 0.5 },
             .{ -0.3, -0.1 },
             .{ -0.2, -0.4 },
@@ -82,7 +73,9 @@ pub const Net = struct {
         });
 
         try net.links.appendNTimes(Link.init(null), net.link_indices.items.len / 2);
-        try net.velocities.appendNTimes(vec.splat2(0), net.positions.items.len);
+        try net.points.appendNTimes(Point.init(null, null), net.point_positions.items.len);
+        net.points.items[1].pin = true;
+        net.points.items[5].pin = true;
 
         return net;
     }
@@ -90,8 +83,8 @@ pub const Net = struct {
     pub fn kill(net: *Net) void {
         net.links.deinit();
         net.link_indices.deinit();
-        net.positions.deinit();
-        net.velocities.deinit();
+        net.points.deinit();
+        net.point_positions.deinit();
     }
 
     // Get the 2 link indices from the link ID
@@ -104,15 +97,18 @@ pub const Net = struct {
     // Get the 2 link vertex positions from the link ID
     fn getLinkPos(net: *Net, link_id: usize) struct { l: *vec.Vec2, r: *vec.Vec2 } {
         const indices = net.getLinkIndices(link_id);
-        const positions = net.positions.items;
+        const positions = net.point_positions.items;
         return .{ .l = &positions[indices.l], .r = &positions[indices.r] };
     }
 
     // Get the 2 link vertex velocities from the link ID
     fn getLinkVel(net: *Net, link_id: usize) struct { l: *vec.Vec2, r: *vec.Vec2 } {
         const indices = net.getLinkIndices(link_id);
-        const velocities = net.velocities.items;
-        return .{ .l = &velocities[indices.l], .r = &velocities[indices.r] };
+        const velocities = net.points.items;
+        return .{
+            .l = &velocities[indices.l].vel,
+            .r = &velocities[indices.r].vel,
+        };
     }
 
     pub fn simulate(net: *Net, delta: f32) void {
@@ -124,11 +120,11 @@ pub const Net = struct {
             const acc = vec.norm2(diff) * vec.splat2(force * proportion * delta);
 
             const velocities = net.getLinkVel(i);
-            velocities.l.* += acc;
-            velocities.r.* -= acc;
+            velocities.l.* -= acc;
+            velocities.r.* += acc;
         }
-        for (net.positions.items, net.velocities.items) |*pos, *vel| {
-            pos.* += vec.splat2(delta) * vel.*;
+        for (net.points.items, net.point_positions.items) |*point, *pos| {
+            if (!point.pin) pos.* += vec.splat2(delta) * point.vel;
         }
     }
 };
@@ -182,7 +178,7 @@ pub fn main() !void {
     // main loop
     while (try gfx.window.ok()) {
         net.simulate(gfx.window.delta);
-        try gfx.mesh.upload(.{net.positions.items});
+        try gfx.mesh.upload(.{net.point_positions.items});
 
         // physics – links between particles
         // reads pos, affects vel
